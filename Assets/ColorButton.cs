38c672efef27c62c391edfeb723567184e9ed8b0
Marie LEGRAND <mlegrand@artfx.fr>
2023-12-06 10:31:36 +0100

    [add] button changing color

---
+++ Assets/ColorButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorButton : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(delegate () { GetComponent<Image>().color = Random.ColorHSV(); });
+    }
+
+}
