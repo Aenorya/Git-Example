96aa687aeb34c363b4cb62dccd0ab464ce844fe7
Marie LEGRAND <mlegrand@artfx.fr>
2023-12-06 10:39:37 +0100

    Update ColorButton.cs

--- Assets/ColorButton.cs
+++ Assets/ColorButton.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class ColorButton : MonoBehaviour
 {
+    public Color color;
     void Start()
     {
         GetComponent<Button>().onClick.AddListener(delegate () { GetComponent<Image>().color = Random.ColorHSV(); });
